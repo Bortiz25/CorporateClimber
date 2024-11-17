2105b808f8c733f52b6d850b72beb300a0746b68
Brandon Ortiz <ortizbrandon140@gmail.com>
2024-11-17 13:40:49 -0600

    shooting based on mousePos/need to fix a speed issue but still works

--- Assets/Scripts/Player/PlayerShootingScript.cs
+++ Assets/Scripts/Player/PlayerShootingScript.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class PlayerShootingScript : MonoBehaviour
 {
     // bullet speed for shooting
-    private float bulletSpeed = 9;
+    private float bulletSpeed = 20;
     // Subject to change the way that the prefab is detected
     public Rigidbody2D bulletPrefab;
     // direction of bullet based on the player movement 
-    private Vector2 shootingDirection; 
+    private Vector2 shootingDirection;
     // playerMovementScript used to getPlayerMovement
     private PlayerManagementScript playerMovement; 
     // boolean to determine if player has picked up a weapon
     public bool hasWeapon;
     public float bulletAmt;
+    private Camera mainCamera;
+    private Vector3 mousePosition; 
 
     void Start(){
         playerMovement = gameObject.GetComponent<PlayerManagementScript>();
@@ -26,17 +29,19 @@ public class PlayerShootingScript : MonoBehaviour
     void Update()
     {
         // updating the direction we are shooting based on movement 
-        shootingDirection = playerMovement.GetMovementVector();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        shootingDirection = new Vector2((mousePosition - gameObject.transform.position).normalized.x, (mousePosition- gameObject.transform.position).normalized.y);
         // calling shooting method
         Shoot();
     }
 
     private void Shoot(){
         if(Input.GetKeyDown(KeyCode.Space) && hasWeapon && bulletAmt != 0){
-            Debug.Log("Player Shooting Activated");
+            Debug.Log("shootingDirection: " + shootingDirection);
             Rigidbody2D bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            bullet.velocity = shootingDirection * bulletSpeed;
-            bullet.AddForce(bulletSpeed * shootingDirection, ForceMode2D.Impulse);
+            bullet.velocity = shootingDirection*bulletSpeed;
+            bullet.AddForce(bulletSpeed*shootingDirection, ForceMode2D.Impulse);
             bulletAmt--;
         }
     }
