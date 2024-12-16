80d3c0c6f5eca605436333bbe94ace1d2104770e
Kinsey Neas <kneas@trinity.edu>
2024-12-16 00:38:31 -0600

    Level 1 and intro level enemies added

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -34,7 +34,7 @@ public class EnemyMovement : MonoBehaviour
             // Determines next path point to move towards
             var targetPosition = pathPoints[pathPointsListIndex].position;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, defaultSpeed*Time.deltaTime);
-            transform.up = targetPosition - new Vector3(transform.position.x, transform.position.y,0f);
+            //transform.up = targetPosition - new Vector3(transform.position.x, transform.position.y,0f);
             // Increments pathPointListIndex when a point in the path has been reached
             if(transform.position == targetPosition) pathPointsListIndex++;
 
