b05ee1d0721d0d1f249ed5557b2f6398e13b8705
Seth Owirodu <sowirodu@trinity.edu>
2024-12-16 04:52:21 -0600

    Transitions

--- Assets/Scripts/Miniboss/MinibossManagementScript.cs
+++ Assets/Scripts/Miniboss/MinibossManagementScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MinibossManagementScript : MonoBehaviour
 {
 
@@ -10,19 +10,31 @@ public class MinibossManagementScript : MonoBehaviour
     public float damage = 0.2f;
     private bool isAlive = true;
     private AudioSource hurtSound;
+    // Reference to the GameManager
+    private GameManager gameManager;
 
-    // Start is called before the first frame update
     void Start()
-    {   
+    {
         // Audio Source for hurt noises
         hurtSound = GameObject.Find("HurtSound").GetComponent<AudioSource>();
+
+        // Either assign via Inspector or find the GameManager in the scene
+        // If your GameManager is persistent and unique, this will work:
+        gameManager = FindObjectOfType<GameManager>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        //health management
+        // Health management
         healthBar.transform.localScale = new Vector3(health, 0.13f, 1);
+        Debug.Log("Boss health: " + health);
+        // Check if the boss is dead
+        if (health <= 0 && isAlive)
+        {
+            OnDeath();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -37,4 +49,28 @@ public class MinibossManagementScript : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+
+
+    private void OnDeath()
+    {
+        isAlive = false;
+        Debug.Log("Boss defeated!");
+
+        // Disable boss components if needed:
+        // GetComponent<Collider2D>().enabled = false;
+        // Disable AI scripts here, if any.
+
+        // Notify the GameManager to handle the scene transition
+        // The GameManager checks boss health and scene logic in CheckMiniBossDone()
+        if (gameManager != null)
+        {
+            gameManager.CheckMiniBossDone();
+        }
+
+        // Optionally, destroy the boss object after some delay if not needed anymore
+        Destroy(gameObject, 2f);
+    }
+
+
+
 }
